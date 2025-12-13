e16766f55134bab279fc177af0ec82caaa7933d4
Kzh2525 <KTC25A31E0015@edu.kyoto-tech.ac.jp>
2025-12-13 16:28:05 +0900

    ページコンポーネントのスタイルを整理し、CSSクラスを追加

--- app/page.tsx
+++ app/page.tsx
@@ -13,3 +13,22 @@ export default function Home() {
     </section>
    ); 
 }
+.top {
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: 
+rgba(0, 0, 0, 0.5);
+  color: #fff;
+  overflow: hidden;
+  padding: 200px 0;
+}
+
+.title {
+  font-size: 3rem;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 1rem;
+
+}

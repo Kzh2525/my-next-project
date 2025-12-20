fb738c41980e3a1b293ea357ba9df7b65b8b030c
Kzh2525 <KTC25A31E0015@edu.kyoto-tech.ac.jp>
2025-12-20 15:43:52 +0900

    トップセクションのスタイルを追加し、HTML構造を整えました

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-
+    <section className={styles.top}></section>
     <div>
       <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
       <p className={styles.description}>
         私たちは市場をリードしているグローバルテックカンパニーです。
         </p>
       </div>
-      
+      <img className={styles.bgimg} src="/img-mv.jpg" alt="" />
+    </section>
   ); 
 }
